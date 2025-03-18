87821ca26769bf8c7dc4c678d95d5ba77dda2c2f
ZeroRowArray <40322081+ZeroRowArray@users.noreply.github.com>
2025-03-18 01:36:01 -0500

    Update make_schema_and_tables.sql
    indention

--- scripts/make_schema_and_tables.sql
+++ scripts/make_schema_and_tables.sql
@@ -3,15 +3,16 @@ create schema bronze;
 create schema silver;
 create schema gold;
 CREATE TABLE bronze.CWIT_buy1 (
-buy_date DATE,	
-buy_time TIME,	
-buy_ticker NVARCHAR(50),
-buy_trans nvarchar(50),
-buy_io nvarchar(3),
-buy_asset_price decimal(9,6),
-buy_num_asset INT,
-buy_inv_rsk decimal(9,6),
-buy_chain NVARCHAR(75),
-buy_wallet_cause NVARCHAR(75),
-buy_wallet_effect NVARCHAR(75)
+	buy_date DATE,	
+	buy_time TIME,	
+	buy_ticker NVARCHAR(50),
+	buy_trans nvarchar(50),
+	buy_io nvarchar(3),
+	buy_asset_price decimal(9,6),
+	buy_num_asset INT,
+	buy_inv_rsk decimal(9,6),
+	buy_chain NVARCHAR(75),
+	buy_wallet_cause NVARCHAR(75),
+	buy_wallet_effect NVARCHAR(75)
+
 )
